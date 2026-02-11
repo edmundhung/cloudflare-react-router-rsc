9fefb041bb4706c662bb8b9608cb4c23c74af081
Edmund Hung <edmund@cloudflare.com>
2026-02-11 18:38:30 +0000

    add an example for using vars in server component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,14 @@
+import { env } from "cloudflare:workers";
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+	return [
+		{ title: "New React Router App" },
+		{ name: "description", content: "Welcome to React Router!" },
+	];
 }
 
 export function ServerComponent() {
-  return <Welcome />;
+	return <Welcome discordURL={env.DISCORD_URL} />;
 }
